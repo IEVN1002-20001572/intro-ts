42ffa1e9b1080eeb6f7f45ef4c64b2e132baf754
Oscar Fernando Barajas Fuentes <20001572@alumnos.utleon.edu.mx>
2024-09-11 21:00:13 -0600

    Trabajo miercoles 11 de septiembre

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 //import './temas-ts/02-interfaz-arre.ts'
-import './temas-ts/03-funciones.ts'
+//import './temas-ts/03-funciones.ts'
+import './temas-ts/05-desestructura-funciones.ts'
+
+
+export function calcularIVA2(productos:Producto[]):[number,number]{
+  let total=0;
+  productos.forEach(({precio})=>{
+    total+=precio
+  });
+  return [total,total*0.16]
+}
+
+const articulo2=[telefono, tablet];
+const [total,iva2]=calcularIVA2(articulo2);
+console.log(`Total: ${total}`);
+console.log(`IVA: ${iva2}`);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
