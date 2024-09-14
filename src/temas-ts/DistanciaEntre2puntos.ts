647fbe09ca91b13271a9aecb31a9abef53f4145c
Oscar Fernando Barajas Fuentes <20001572@alumnos.utleon.edu.mx>
2024-09-14 10:29:01 -0600

    COMMIT FINAL

--- src/temas-ts/DistanciaEntre2puntos.ts
+++ src/temas-ts/DistanciaEntre2puntos.ts
@@ -1,42 +1,33 @@
-
 export class Punto {
-    protected x: number;
-    protected y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-
-    imprimir(): void {
-        console.log(`Punto: (${this.x}, ${this.y})`);
-    }
-}
-
-export class Distancia extends Punto {
+    protected x1: number;
+    protected y1: number;
     protected x2: number;
     protected y2: number;
 
     constructor(x: number, y: number, x2: number, y2: number) {
-        super(x, y);
+        this.x1 = x;
+        this.y1 = y;
         this.x2 = x2;
         this.y2 = y2;
     }
-
     calcularDistancia(): void {
-        const distancia = Math.sqrt(Math.pow(this.x2 - this.x, 2) + Math.pow(this.y2 - this.y, 2));
-        console.log(`La distancia entre los puntos (${this.x}, ${this.y}) y (${this.x2}, ${this.y2}) es: ${distancia}`);
+        const distancia = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2));
+        console.log(`La distancia entre los puntos (${this.x1}, ${this.y1}) y (${this.x2}, ${this.y2}) es: ${distancia}`);
     }
 
+ 
     imprimir(): void {
-        console.log(`Punto 1: (${this.x}, ${this.y}) Punto 2: (${this.x2}, ${this.y2})`);
+        console.log(`Punto 1: (${this.x1}, ${this.y1}) Punto 2: (${this.x2}, ${this.y2})`);
     }
 }
 
+const x1 = 2;
+const y1 = 9;
+const x2 = 3;
+const y2 = 5;
 
-const punto1 = new Punto(3, 4);
+const punto1 = new Punto(x1, y1, x2, y2);
 punto1.imprimir();
+punto1.calcularDistancia(); 
 
-const distancia1 = new Distancia(3, 4, 7, 1);
-distancia1.imprimir();
-distancia1.calcularDistancia();
+export { x1, y1, x2, y2 };
--- src/temas-ts/triangulo.ts
+++ src/temas-ts/triangulo.ts
@@ -1,38 +1,46 @@
+import { Punto, x1, y1, x2, y2 } from './DistanciaEntre2puntos'; 
 
-import { Distancia } from './DistanciaEntre2puntos'; 
-
-class VerificarTriangulo extends Distancia {
+class VerificarTriangulo extends Punto { 
     private x3: number;
     private y3: number;
 
     constructor(x: number, y: number, x2: number, y2: number, x3: number, y3: number) {
-        super(x, y, x2, y2); 
+        super(x, y, x2, y2);  
         this.x3 = x3;
         this.y3 = y3;
     }
 
-    esTriangulo(): void {
-        
-        const area = Math.abs(this.x * (this.y2 - this.y3) + this.x2 * (this.y3 - this.y) + this.x3 * (this.y - this.y2)) / 2;
+    calcularDistanciaEntrePuntos(x1: number, y1: number, x2: number, y2: number): number {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
 
-        if (area > 0) {
-            console.log('Es un triangulo!!!');
+    esTriangulo(): void {
+        const distancia1 = this.calcularDistanciaEntrePuntos(this.x1, this.y1, this.x2, this.y2);
+        const distancia2 = this.calcularDistanciaEntrePuntos(this.x2, this.y2, this.x3, this.y3);
+        const distancia3 = this.calcularDistanciaEntrePuntos(this.x1, this.y1, this.x3, this.y3);
+
+        console.log(`Distancia entre Punto A (${this.x1}, ${this.y1}) y Punto B (${this.x2}, ${this.y2}): ${distancia1}`);
+        console.log(`Distancia entre Punto B (${this.x2}, ${this.y2}) y Punto C (${this.x3}, ${this.y3}): ${distancia2}`);
+        console.log(`Distancia entre Punto A (${this.x1}, ${this.y1}) y Punto C (${this.x3}, ${this.y3}): ${distancia3}`);
+
+        if (distancia1 + distancia2 > distancia3 && distancia2 + distancia3 > distancia1 && distancia1 + distancia3 > distancia2) {
+            console.log('Es un triángulo!!!');
         } else {
-            console.log('No es un triangulo :c');
+            console.log('No es un triángulo :c');
         }
     }
 
     imprimirPuntos(): void {
-        console.log(`Punto 1: (${this.x}, ${this.y})`);
+        console.log(`Punto 1: (${this.x1}, ${this.y1})`);
         console.log(`Punto 2: (${this.x2}, ${this.y2})`);
         console.log(`Punto 3: (${this.x3}, ${this.y3})`);
     }
 }
 
 
-const x3 = Math.floor(Math.random() * 10);
-const y3 = Math.floor(Math.random() * 10);
+const x3 = 3;
+const y3 = 4;
 
-const verificarTriangulo = new VerificarTriangulo(3, 4, 7, 1, x3, y3); 
+const verificarTriangulo = new VerificarTriangulo(x1, y1, x2, y2, x3, y3);
 verificarTriangulo.imprimirPuntos();
 verificarTriangulo.esTriangulo();
